eb0fb157e36bfba61fb76620facabd118bbc87d6
Aanchallll18 <mahatoaachal4@gmail.com>
2024-12-27 16:04:01 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import Navbar from "./components/Navbar"
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
 
 export default function App() {
-	return
-   (
+  return (
     <>
-      <Navbar/>
+      <Navbar />
+      <Hero />
+      <Skills/>
     </>
-   )
+  );
 }
