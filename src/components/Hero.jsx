e714b36ab7579c72497a9cfba56d2b7f4fb845c2
Aanchallll18 <kumariaachal002@gmail.com>
2024-12-31 23:39:35 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -73,16 +73,23 @@ const Hero = () => {
 								Download CV
 							</a>
 						</motion.button>
+
 						<div className="flex gap-6 flex-row text-4xl md:text-purple-400 z-20">
-							<motion.a whileHover={{ scale: 1.2 }} href="https://github.com/Aanchalll18">
+							<motion.a whileHover={{ scale: 1.2 }} href="https://github.com/Aanchalll18"
+							className="text-purple-900"
+							>
 								<AiOutlineGithub />
 							</motion.a>
 
-							<motion.a whileHover={{ scale: 1.2 }} href="https://www.linkedin.com/in/aanchal-161b26270/">
+							<motion.a whileHover={{ scale: 1.2 }} href="https://www.linkedin.com/in/aanchal-161b26270/"
+							className="  text-purple-900"
+							>
 								<AiOutlineLinkedin />
 							</motion.a>
 
-							<motion.a whileHover={{ scale: 1.2 }} href="mailto:kumariaachal002@gmail.com">
+							<motion.a whileHover={{ scale: 1.2 }} href="mailto:kumariaachal002@gmail.com"
+							className="  text-purple-900"
+							>
 								<AiOutlineMail />
 							</motion.a>
 						</div>
